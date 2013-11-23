0ec6380811035b23ac31fc11e284e0dac3215a49
randombox <rymur@live.com>
2013-11-23 09:45:06 -0500

    Changed toString

--- CommentPost.java
+++ CommentPost.java
@@ -1,13 +1,11 @@
 public class CommentPost extends Post {
 
-	public CommentPost(int id,String date, String text, String usname) { 
-		super(id,date,text,usname);
+	public CommentPost(int id,String date, String text, String user) { 
+		super(id,date,text,user);
 	}
 
 	public String toString()
 	{
-		String a = "ID:"+getID()+" Date:"+getDate()+" UserName:"+getUser()+ "\n";
-		a += "Comment:"+getText()+"\n";
-		return a;
+		return super.toString();
 	}
 }
