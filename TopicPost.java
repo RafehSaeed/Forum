ac3f34e2c9a2f613525cf9db95180c159c0f843f
randombox <rymur@live.com>
2013-11-23 09:43:53 -0500

    Fixed toString, added getComments

--- TopicPost.java
+++ TopicPost.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class TopicPost extends Post {
 
-	String title;
-	ArrayList<CommentPost> cmpost;
+	private String title;
+	private ArrayList<CommentPost> cmpost;
 
 	public TopicPost(int id,String date, String text, String user, String title) { 
 		super(id,date,text,user);
@@ -25,39 +25,50 @@ public class TopicPost extends Post {
 	{
 		int start = 0;
 		int end = cmpost.size() - 1;
+		int index = searchComment(id, start, end);
 		
-		return removeComment(id, start, end);
+		if(index == -1) {
+			return false;
+		} else {
+			cmpost.remove(index);
+			return true;
+		}
 	}
 	
-	private boolean removeComment(int id, int start, int end)
+	private int searchComment(int id, int start, int end)
 	{
 		if((end-start) < 0)
-			return false;
+			return -1;
 		
 		int middle = (end - start)/2;
 		CommentPost cp = cmpost.get(middle);
 		
 		if(cp.getID() == id) {
-			cmpost.remove(middle);
-			return true;
+			return middle;
 		} else if(cp.getID() < id) {
-			return removeComment(id, middle+1, end);
+			return searchComment(id, middle+1, end);
 		} else {
-			return removeComment(id, start, middle-1);
+			return searchComment(id, start, middle-1);
 		}
 	}
 
 	public String toString()
 	{
-		String a = "Topic:"+title+"\n";
-		for(int i=0 ; i < cmpost.size();i++)
-		{
-			a+= cmpost.get(i).toString();
-		}
+		String a = "Topic: "+title+"\n";
+		a += super.toString();
 		
 		return a;
 	}
-
-
+	
+	public String getComments() {
+		String comments = "";
+		
+		for(int i=0 ; i < cmpost.size();i++)
+		{
+			comments += cmpost.get(i).toString()+"\n";
+		}
+		
+		return comments;
+	}
 
 }
