627e3d228af0a2f674c7f282021e9d62ca6b0a49
Rafeh <rafeh_saeed@hotmail.ca>
2013-11-23 12:59:24 -0500

    tostringgetcommentsshudbesame

--- TopicPost.java
+++ TopicPost.java
@@ -2,73 +2,73 @@ import java.util.*;
 
 public class TopicPost extends Post {
 
-	private String title;
-	private ArrayList<CommentPost> cmpost;
+ private String title;
+ private ArrayList<CommentPost> cmpost;
 
-	public TopicPost(int id,String date, String text, String user, String title) { 
-		super(id,date,text,user);
-		this.title = title;
-		cmpost = new ArrayList<CommentPost>();
-	}
+ public TopicPost(int id,String date, String text, String user, String title) { 
+  super(id,date,text,user);
+  this.title = title;
+  cmpost = new ArrayList<CommentPost>();
+ }
 
-	public String getTitle()
-	{
-		return title;
-	}
+ public String getTitle()
+ {
+  return title;
+ }
 
-	public void addComment(CommentPost comment)
-	{
-		cmpost.add(comment);
-	}
+ public void addComment(CommentPost comment)
+ {
+  cmpost.add(comment);
+ }
 
-	public boolean removeComment(int id)
-	{
-		int start = 0;
-		int end = cmpost.size() - 1;
-		int index = searchComment(id, start, end);
-		
-		if(index == -1) {
-			return false;
-		} else {
-			cmpost.remove(index);
-			return true;
-		}
-	}
-	
-	private int searchComment(int id, int start, int end)
-	{
-		if((end-start) < 0)
-			return -1;
-		
-		int middle = (end - start)/2;
-		CommentPost cp = cmpost.get(middle);
-		
-		if(cp.getID() == id) {
-			return middle;
-		} else if(cp.getID() < id) {
-			return searchComment(id, middle+1, end);
-		} else {
-			return searchComment(id, start, middle-1);
-		}
-	}
+ public boolean removeComment(int id)
+ {
+  int start = 0;
+  int end = cmpost.size() - 1;
+  int index = searchComment(id, start, end);
+  
+  if(index == -1) {
+   return false;
+  } else {
+   cmpost.remove(index);
+   return true;
+  }
+ }
+ 
+ private int searchComment(int id, int start, int end)
+ {
+  if((end-start) < 0)
+   return -1;
+  
+  int middle = (end - start)/2;
+  CommentPost cp = cmpost.get(middle);
+  
+  if(cp.getID() == id) {
+   return middle;
+  } else if(cp.getID() < id) {
+   return searchComment(id, middle+1, end);
+  } else {
+   return searchComment(id, start, middle-1);
+  }
+ }
 
-	public String toString()
-	{
-		String a = "Topic: "+title+"\n";
-		a += super.toString();
-		
-		return a;
-	}
-	
-	public String getComments() {
-		String comments = "";
-		
-		for(int i=0 ; i < cmpost.size();i++)
-		{
-			comments += cmpost.get(i).toString()+"\n";
-		}
-		
-		return comments;
-	}
+ public String toString() // toString getcomments should be one method
+ {
+  String a = "Topic: "+title+"\n";
+  a += super.toString();
+  a += getComments();//We dont need an extra method to do this as everytime we will print topic all comments will be printed
+  return a;
+ }
+ 
+ public String getComments() {
+  String comments = "";
+  
+  for(int i=0 ; i < cmpost.size();i++)
+  {
+   comments += cmpost.get(i).toString()+"\n";
+  }
+  
+  return comments;
+ }
 
 }
